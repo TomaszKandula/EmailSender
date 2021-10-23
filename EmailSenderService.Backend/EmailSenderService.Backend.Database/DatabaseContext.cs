aa131f599ff2690adb40a8f3a78dedf5bf1ea6bf
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-10-23 21:43:20 +0200

    feat: add database context

---
+++ EmailSenderService.Backend/EmailSenderService.Backend.Database/DatabaseContext.cs
@@ -0,0 +1,30 @@
+﻿namespace EmailSenderService.Backend.Database
+{
+    using System.Reflection;
+    using System.Diagnostics.CodeAnalysis;
+    using Microsoft.EntityFrameworkCore;
+    using Domain.Entities;
+
+    [ExcludeFromCodeCoverage]
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+
+        public virtual DbSet<Email> Email { get; set; }
+
+        public virtual DbSet<User> User { get; set; }
+
+        public virtual DbSet<History> History { get; set; }
+
+        public virtual DbSet<RegisteredEmail> RegisteredEmail { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            ApplyConfiguration(modelBuilder);
+        }
+
+        private static void ApplyConfiguration(ModelBuilder modelBuilder) 
+            => modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
