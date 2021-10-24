ed8c7fc3e19869aff40b98e382e3d252c95a3905
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-10-24 23:23:03 +0200

    feat: add new validation rules

--- EmailSenderService.WebApi/Validators/SendEmailDtoValidator.cs
+++ EmailSenderService.WebApi/Validators/SendEmailDtoValidator.cs
@@ -11,12 +11,60 @@ namespace EmailSenderService.WebApi.Validators
             RuleFor(dto => dto.From)
                 .NotEmpty()
                 .WithErrorCode(nameof(ValidationCodes.REQUIRED))
-                .WithMessage(ValidationCodes.REQUIRED);
+                .WithMessage(ValidationCodes.REQUIRED)
+                .EmailAddress()
+                .WithErrorCode(nameof(ValidationCodes.INVALID_EMAIL_ADDRESS))
+                .WithMessage(ValidationCodes.INVALID_EMAIL_ADDRESS);
 
             RuleFor(dto => dto.Subject)
                 .NotEmpty()
                 .WithErrorCode(nameof(ValidationCodes.REQUIRED))
                 .WithMessage(ValidationCodes.REQUIRED);
+
+            RuleFor(dto => dto.To)
+                .NotEmpty()
+                .WithErrorCode(nameof(ValidationCodes.REQUIRED))
+                .WithMessage(ValidationCodes.REQUIRED);
+
+            RuleForEach(dto => dto.To)
+                .NotEmpty()
+                .WithErrorCode(nameof(ValidationCodes.REQUIRED))
+                .WithMessage(ValidationCodes.REQUIRED)
+                .EmailAddress()
+                .WithErrorCode(nameof(ValidationCodes.INVALID_EMAIL_ADDRESS))
+                .WithMessage(ValidationCodes.INVALID_EMAIL_ADDRESS);
+
+            When(dto => dto.Cc != null, () =>
+            {
+                RuleFor(dto => dto.Cc)
+                    .NotEmpty()
+                    .WithErrorCode(nameof(ValidationCodes.REQUIRED))
+                    .WithMessage(ValidationCodes.REQUIRED);
+
+                RuleForEach(dto => dto.Cc)
+                    .NotEmpty()
+                    .WithErrorCode(nameof(ValidationCodes.REQUIRED))
+                    .WithMessage(ValidationCodes.REQUIRED)
+                    .EmailAddress()
+                    .WithErrorCode(nameof(ValidationCodes.INVALID_EMAIL_ADDRESS))
+                    .WithMessage(ValidationCodes.INVALID_EMAIL_ADDRESS);
+            });
+
+            When(dto => dto.Bcc != null, () =>
+            {
+                RuleFor(dto => dto.Bcc)
+                    .NotEmpty()
+                    .WithErrorCode(nameof(ValidationCodes.REQUIRED))
+                    .WithMessage(ValidationCodes.REQUIRED);
+
+                RuleForEach(dto => dto.Bcc)
+                    .NotEmpty()
+                    .WithErrorCode(nameof(ValidationCodes.REQUIRED))
+                    .WithMessage(ValidationCodes.REQUIRED)
+                    .EmailAddress()
+                    .WithErrorCode(nameof(ValidationCodes.INVALID_EMAIL_ADDRESS))
+                    .WithMessage(ValidationCodes.INVALID_EMAIL_ADDRESS);
+            });
         }
     }
 }
