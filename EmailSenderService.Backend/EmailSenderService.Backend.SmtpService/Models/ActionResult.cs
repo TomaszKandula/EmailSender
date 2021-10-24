1c0492acd4d9f36bfb2495922c7db1cbbf9edbb4
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-10-24 16:03:57 +0200

    feat: add new models

---
+++ EmailSenderService.Backend/EmailSenderService.Backend.SmtpService/Models/ActionResult.cs
@@ -0,0 +1,16 @@
+﻿namespace EmailSenderService.Backend.SmtpService.Models
+{
+    using System.Diagnostics.CodeAnalysis;
+
+    [ExcludeFromCodeCoverage]
+    public class ActionResult
+    {
+        public bool IsSucceeded { get; set; }
+
+        public string ErrorCode { get; set; }
+        
+        public string ErrorDesc { get; set; }
+        
+        public string InnerMessage { get; set; }
+    }
+}
