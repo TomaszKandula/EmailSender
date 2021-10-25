3bec81053c1812cf80ffd79b88b2a26183c5f46f
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-10-25 20:58:39 +0200

    refactor: upgrade implementation

--- EmailSenderService.Backend/EmailSenderService.Backend.SmtpService/ISmtpClientService.cs
+++ EmailSenderService.Backend/EmailSenderService.Backend.SmtpService/ISmtpClientService.cs
@@ -7,26 +7,16 @@
 
     public interface ISmtpClientService
     {
-        string From { get; set; }
+        EmailData EmailData { get; set; }
 
-        List<string> Tos { get; set; }
-        
-        List<string> Ccs { get; set; }
-        
-        List<string> Bccs { get; set; }
-        
-        string Subject { get; set; }
-        
-        string PlainText { get; set; }
-        
-        string HtmlBody { get; set; }
+        ServerData ServerData { get; set; }
 
-        Task<ActionResult> CanConnectAndAuthenticate(CancellationToken cancellationToken = default);
-        
+        Task<ActionResult> VerifyConnection(CancellationToken cancellationToken = default);
+
         Task<ActionResult> Send(CancellationToken cancellationToken = default);
-        
-        List<Email> IsAddressCorrect(IEnumerable<string> emailAddress);
-        
+
+        IDictionary<string, bool> IsAddressCorrect(IEnumerable<string> emailAddress);
+
         Task<bool> IsDomainCorrect(string emailAddress, CancellationToken cancellationToken = default);
     }
 }
