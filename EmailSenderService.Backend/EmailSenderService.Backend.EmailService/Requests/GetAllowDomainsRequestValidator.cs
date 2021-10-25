d4f3782e3128157d8dc5459ba24e7101ac0aaadc
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-10-25 23:34:24 +0200

    feat: add validation rule for property

---
+++ EmailSenderService.Backend/EmailSenderService.Backend.EmailService/Requests/GetAllowDomainsRequestValidator.cs
@@ -0,0 +1,16 @@
+namespace EmailSenderService.Backend.EmailService.Requests
+{
+    using FluentValidation;
+    using Shared.Resources;
+
+    public class GetAllowDomainsRequestValidator : AbstractValidator<GetAllowDomainsRequest>
+    {
+        public GetAllowDomainsRequestValidator()
+        {
+            RuleFor(request => request.PrivateKey)
+                .NotEmpty()
+                .WithErrorCode(nameof(ValidationCodes.REQUIRED))
+                .WithMessage(ValidationCodes.REQUIRED);
+        }
+    }
+}
