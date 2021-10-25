20be9d97db29f6915df2b419e3228383ddac45f5
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-10-25 15:17:19 +0200

    refactor: replace with mediator handler

--- EmailSenderService.WebApi/Controllers/EmailController.cs
+++ EmailSenderService.WebApi/Controllers/EmailController.cs
@@ -1,40 +1,25 @@
 namespace EmailSenderService.WebApi.Controllers
 {
-    using System.Threading;
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Authorization;
     using Backend.Shared.Dto;
-    using Backend.EmailService;
     using Backend.Shared.Attributes;
-    using Backend.EmailService.Models;
+    using Backend.EmailService.Mappers;
+    using MediatR;
 
     [Route("api/v1/[controller]/[action]")]
     [ApiController]
+    [AllowAnonymous]
     public class EmailController : ControllerBase
     {
-        private readonly IEmailService _emailService;
+        private readonly IMediator _mediator;
 
-        public EmailController(IEmailService emailService) => _emailService = emailService;
+        public EmailController(IMediator mediator) => _mediator = mediator;
 
         [HttpPost]
         [ETagFilter(204)]
-        [AllowAnonymous]
-        public async Task<IActionResult> SendEmail([FromBody] SendEmailDto payLoad)
-        {
-            var config = new Configuration
-            {
-                From = payLoad.From,
-                Subject = payLoad.Subject,
-                To = payLoad.To,
-                Cc = payLoad.Cc,
-                Bcc = payLoad.Bcc,
-                Body = payLoad.Body,
-                IsHtml = payLoad.IsHtml
-            };
-
-            await _emailService.Send(config, CancellationToken.None);
-            return NoContent();
-        }
+        public async Task<Unit> SendEmail([FromBody] SendEmailDto payLoad)
+            => await _mediator.Send(EmailMapper.MapToSendEmailRequest(payLoad));
     }
 }
