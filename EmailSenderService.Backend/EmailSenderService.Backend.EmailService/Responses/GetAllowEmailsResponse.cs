02db023f97d4a4ecb5779cf90591b0522dd6f14e
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-10-26 10:11:41 +0200

    feat: add models for new endpoint

---
+++ EmailSenderService.Backend/EmailSenderService.Backend.EmailService/Responses/GetAllowEmailsResponse.cs
@@ -0,0 +1,11 @@
+namespace EmailSenderService.Backend.EmailService.Responses
+{
+    using System.Collections.Generic;
+
+    public class GetAllowEmailsResponse
+    {
+        public string AssociatedUser { get; set; }
+
+        public IEnumerable<string> Emails { get; set; }
+    }
+}
