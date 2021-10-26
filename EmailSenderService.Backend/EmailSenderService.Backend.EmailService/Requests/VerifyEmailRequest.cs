6d5d986d8eaf000c28512abecd2776719c03539f
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-10-26 17:38:29 +0200

    feat: add models for response and request

---
+++ EmailSenderService.Backend/EmailSenderService.Backend.EmailService/Requests/VerifyEmailRequest.cs
@@ -0,0 +1,13 @@
+namespace EmailSenderService.Backend.EmailService.Requests
+{
+    using MediatR;
+    using Responses;
+    using System.Collections.Generic;
+
+    public class VerifyEmailRequest : IRequest<VerifyEmailResponse>
+    {
+        public string PrivateKey { get; set; }
+
+        public IEnumerable<string> Emails { get; set; }
+    }
+}
