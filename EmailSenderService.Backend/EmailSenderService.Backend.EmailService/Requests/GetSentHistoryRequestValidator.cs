67820ff5b167e4bfe71c0ad95f167e92efb65982
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-10-26 10:34:48 +0200

    feat: add new handler and validator for getting history of actions

---
+++ EmailSenderService.Backend/EmailSenderService.Backend.EmailService/Requests/GetSentHistoryRequestValidator.cs
@@ -0,0 +1,16 @@
+namespace EmailSenderService.Backend.EmailService.Requests
+{
+    using FluentValidation;
+    using Shared.Resources;
+
+    public class GetSentHistoryRequestValidator : AbstractValidator<GetSentHistoryRequest>
+    {
+        public GetSentHistoryRequestValidator()
+        {
+            RuleFor(request => request.PrivateKey)
+                .NotEmpty()
+                .WithErrorCode(nameof(ValidationCodes.REQUIRED))
+                .WithMessage(ValidationCodes.REQUIRED);
+        }
+    }
+}
