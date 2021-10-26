a7788553598378ae9b9cc0e7a72ad7fdecddb0d2
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-10-26 10:34:22 +0200

    feat: ad models for new endpoint and handler

---
+++ EmailSenderService.Backend/EmailSenderService.Backend.EmailService/Models/HistoryEntry.cs
@@ -0,0 +1,11 @@
+namespace EmailSenderService.Backend.EmailService.Models
+{
+    using System;
+
+    public class HistoryEntry
+    {
+        public string EmailFrom { get; set; }
+        
+        public DateTime SentAt { get; set; }
+    }
+}
