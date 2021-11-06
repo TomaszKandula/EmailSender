5940fa1ad3b52e5e6de99abfe03df7a6b90de52b
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-11-06 13:31:04 +0100

    feat: add new tests for validators

---
+++ EmailSender.Backend.Tests/Validators/VerifyEmailRequestValidatorTest.cs
@@ -0,0 +1,53 @@
+namespace EmailSender.Backend.Tests.Validators
+{
+    using Xunit;
+    using FluentAssertions;
+    using System.Collections.Generic;
+    using Shared.Resources;
+    using EmailService.Requests;
+
+    public class VerifyEmailRequestValidatorTest : TestBase
+    {
+        [Fact]
+        public void GivenValidInputs_WhenVerifyEmailRequest_ShouldSucceed()
+        {
+            // Arrange
+            var request = new VerifyEmailRequest
+            {
+                PrivateKey = DataUtilityService.GetRandomString(),
+                Emails = new List<string>
+                {
+                    DataUtilityService.GetRandomEmail(),
+                    DataUtilityService.GetRandomEmail()
+                }
+            };
+
+            // Act
+            var validator = new VerifyEmailRequestValidator();
+            var result = validator.Validate(request);
+
+            // Assert
+            result.Errors.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void GivenInvalidInputs_WhenVerifyEmailRequest_ShouldThrowErrors()
+        {
+            // Arrange
+            var request = new VerifyEmailRequest
+            {
+                PrivateKey = string.Empty,
+                Emails = null
+            };
+
+            // Act
+            var validator = new VerifyEmailRequestValidator();
+            var result = validator.Validate(request);
+
+            // Assert
+            result.Errors.Should().HaveCount(2);
+            result.Errors[0].ErrorCode.Should().Be(nameof(ValidationCodes.REQUIRED));
+            result.Errors[1].ErrorCode.Should().Be(nameof(ValidationCodes.REQUIRED));
+        }
+    }
+}
