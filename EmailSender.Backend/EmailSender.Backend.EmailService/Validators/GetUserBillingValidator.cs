6e329f81d5414cc8b59f2c9f984f6e87791901f9
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-11-06 23:50:41 +0100

    fix: ad missing property rule

--- EmailSender.Backend/EmailSender.Backend.EmailService/Validators/GetUserBillingValidator.cs
+++ EmailSender.Backend/EmailSender.Backend.EmailService/Validators/GetUserBillingValidator.cs
@@ -8,6 +8,11 @@ namespace EmailSender.Backend.EmailService.Validators
     {
         public GetUserBillingValidator()
         {
+            RuleFor(request => request.PrivateKey)
+                .NotEmpty()
+                .WithErrorCode(nameof(ValidationCodes.REQUIRED))
+                .WithMessage(ValidationCodes.REQUIRED);
+
             RuleFor(request => request.BillingId)
                 .NotEmpty()
                 .WithErrorCode(nameof(ValidationCodes.REQUIRED))
