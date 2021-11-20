3273de0b039544eb0887d30cdb008dbc1dded8c3
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-11-20 21:04:39 +0100

    refactor: change response body for 403 error

--- EmailSender.WebApi/Middleware/CustomCors.cs
+++ EmailSender.WebApi/Middleware/CustomCors.cs
@@ -4,8 +4,10 @@
     using System.Threading.Tasks;
     using System.Diagnostics.CodeAnalysis;
     using Microsoft.AspNetCore.Http;
-    using Configuration;
     using Backend.UserService;
+    using Backend.Core.Models;
+    using Backend.Shared.Resources;
+    using Newtonsoft.Json;
 
     [ExcludeFromCodeCoverage]
     public class CustomCors
@@ -22,11 +24,11 @@
             if (!allowDomains)
             {
                 httpContext.Response.StatusCode = 403;
-                await httpContext.Response.WriteAsync("Forbidden");
+                var applicationError = new ApplicationError(nameof(ErrorCodes.ACCESS_FORBIDDEN), ErrorCodes.ACCESS_FORBIDDEN);
+                await httpContext.Response.WriteAsync(JsonConvert.SerializeObject(applicationError));
                 return;
             }
 
-            CorsHeaders.Apply(httpContext);
             await _requestDelegate(httpContext);
         }
     }
