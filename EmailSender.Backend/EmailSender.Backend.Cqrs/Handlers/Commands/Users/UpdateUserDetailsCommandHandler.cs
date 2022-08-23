89aeefe5c3b515de05e713db501c34d11d820188
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2022-08-23 11:45:27 +0200

    feat: add cqrs for updating user details

---
+++ EmailSender.Backend/EmailSender.Backend.Cqrs/Handlers/Commands/Users/UpdateUserDetailsCommandHandler.cs
@@ -0,0 +1,40 @@
+namespace EmailSender.Backend.Cqrs.Handlers.Commands.Users;
+
+using System.Threading;
+using System.Threading.Tasks;
+using Services.UserService;
+using Services.UserService.Models;
+using Core.Services.LoggerService;
+using MediatR;
+
+public class UpdateUserDetailsCommandHandler : Cqrs.RequestHandler<UpdateUserDetailsCommand, Unit>
+{
+    private readonly IUserService _userService;
+
+    private readonly ILoggerService _loggerService;
+
+    public UpdateUserDetailsCommandHandler(ILoggerService loggerService, IUserService userService)
+    {
+        _loggerService = loggerService;
+        _userService = userService;
+    }
+
+    public override async Task<Unit> Handle(UpdateUserDetailsCommand request, CancellationToken cancellationToken)
+    {
+        var payload = new UserCompanyInfo
+        {
+            UserId = request.UserId,
+            CompanyName = request.CompanyName, 
+            VatNumber = request.VatNumber,
+            StreetAddress = request.StreetAddress,
+            PostalCode = request.PostalCode,
+            Country = request.Country,
+            City= request.City
+        };
+
+        await _userService.UpdateUserDetails(payload, cancellationToken);
+        _loggerService.LogInformation($"User account has been updated, user ID: {request.UserId}");
+
+        return Unit.Value;
+    }
+}
