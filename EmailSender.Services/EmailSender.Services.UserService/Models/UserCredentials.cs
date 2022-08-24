df2add65a0d43cd637b5e9b7ff3460e79038500d
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2022-08-24 21:26:38 +0200

    feat: remove property

--- EmailSender.Services/EmailSender.Services.UserService/Models/UserCredentials.cs
+++ EmailSender.Services/EmailSender.Services.UserService/Models/UserCredentials.cs
@@ -1,14 +1,11 @@
 namespace EmailSender.Services.UserService.Models;
 
-using System;
 using System.Diagnostics.CodeAnalysis;
 using Backend.Domain.Enums;
 
 [ExcludeFromCodeCoverage]
 public class UserCredentials
 {
-    public Guid UserId { get; set; }
-
     public string PrivateKey { get; set; }
 
     public string UserAlias { get; set; }
