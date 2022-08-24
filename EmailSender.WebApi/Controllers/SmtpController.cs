d0a0bbb0ec2b2b594b54a7a5ce8da4a1ba4534dd
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2022-08-24 17:22:34 +0200

    feat: add billable attribute to the selected endpoints

--- EmailSender.WebApi/Controllers/SmtpController.cs
+++ EmailSender.WebApi/Controllers/SmtpController.cs
@@ -3,6 +3,7 @@ namespace EmailSender.WebApi.Controllers;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
+using Backend.Shared.Attributes;
 using Backend.Cqrs.Handlers.Queries.Smtp;
 using MediatR;
 
@@ -12,6 +13,7 @@ public class SmtpController : BaseController
     public SmtpController(IMediator mediator) : base(mediator) { }
 
     [HttpGet]
+    [BillableEndpoint]
     [ProducesResponseType(typeof(Unit), StatusCodes.Status200OK)]
     public async Task<Unit> GetServerStatus([FromQuery] string emailAddress, [FromHeader(Name = HeaderName)] string privateKey) 
         => await Mediator.Send(new GetServerStatusQuery { EmailAddress = emailAddress });
