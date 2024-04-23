15286b0af0b3af0dd05e4a8a2e564f8e02cfca0a
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2024-04-23 19:16:28 +0200

    feat: remove unnecessary code

--- docker-compose-up-prod.sh
+++ docker-compose-up-prod.sh
@@ -4,13 +4,11 @@ cp docker-compose-prod.yml docker-compose-run.yml
 cp EmailSender.ClientApp/nginx/nginx-https.conf EmailSender.ClientApp/nginx/nginx.conf
 
 # SET ENVIRONMENT VARIABLES
-APP_BACKEND="emailsender.dev"
 SERVER_NAME="emailsender.dev"
 
 # APPLY ENVIRONMENT VARIABLES
 sed -i \
 -e "s/\${SERVER_NAME}/${SERVER_NAME}/" \
--e "s/\${APP_BACKEND}/${APP_BACKEND}/" \
 EmailSender.ClientApp/nginx/nginx.conf 
 
 # RUN DOCKER COMPOSE
